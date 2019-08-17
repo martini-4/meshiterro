ddbecfb63612fc70e0aa489f97cd1d042f9f728d
吉松拓巳 <matty@matty-MacBook-Pro.local>
2019-08-18 00:14:07 +0900

    [Finish]6-6

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '4bdc1da2ecf5cb666a1c634fc27a129bb556f502bf1bdc50cbd9931e75829e3384d828fff716fd4c0128072c3b1546c281bd748b2ca3a45da35c01e7fe48c728'
